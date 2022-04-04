d1b96eefc03c10235bff2761d537c9accda735dc
Renan Noronha <renancnoronha@gmail.com>
2022-04-04 16:37:11 -0300

    env base url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: process.env.BASE_URL_API,
+    baseURL: process.env.BASE_URL_API || 'http://localhost:3001/',
 });
+
+console.log(process.env);
+console.log(process.env.BASE_URL_API);
 
 export default api;
