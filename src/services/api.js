78f3443f05c8df89f743377127cf48400d9e7c31
Renan Noronha <renancnoronha@gmail.com>
2022-04-04 16:50:02 -0300

    REACT_APP_  prefix

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-
 const api = axios.create({
-    baseURL: process.env.BASE_URL_API || 'http://localhost:3001/',
+    baseURL: process.env.REACT_APP_BASE_URL_API,
 });
-
-console.log(process.env);
-console.log(process.env.BASE_URL_API);
 
 export default api;
