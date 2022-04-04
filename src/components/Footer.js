76563a4b7e8e3325dde74aeefe5fca0864e20c68
Renan Noronha <renancnoronha@gmail.com>
2022-04-04 09:12:09 -0300

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer className="text-muted">
+            <div className="container">
+                <p className="float-right">
+                    <a href="#">Back to top</a>
+                </p>
+                <p>© 2022 Renan Noronha</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
