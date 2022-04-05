e438229ae919dc86e41e49e5b2af55f6aa6d0e97
Renan Noronha <renancnoronha@gmail.com>
2022-04-05 17:16:32 -0300

    typo fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./Card.css";
 
-function Card() {
+function Card(props) {
     const options = {
         year: "numeric",
         month: "numeric",
@@ -13,21 +13,21 @@ function Card() {
     };
     return (
         <div className="card mb-4 box-shadow">
-            <img className="card-img-top" src={this.props.src ? this.props.src : "placeholder_image.jpg"} alt={this.props.alt} />
+            <img className="card-img-top" src={props.src ? props.src : "placeholder_image.jpg"} alt={props.alt} />
             <div className="card-body">
-                <h5>{this.props.title}</h5>
-                <p className="card-text">{this.props.details}</p>
+                <h5>{props.title}</h5>
+                <p className="card-text">{props.details}</p>
                 <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
-                        {this.props.live && (
-                            <a target="_blank" rel="noreferrer" href={this.props.live}>
+                        {props.live && (
+                            <a target="_blank" rel="noreferrer" href={props.live}>
                                 <button type="button" className="btn btn-sm btn-outline-secondary">
                                     Assistir Lançamento
                                 </button>
                             </a>
                         )}
                     </div>
-                    <small className="text-muted">{this.props.date ? new Date(this.props.date).toLocaleDateString([], options) : ""}</small>
+                    <small className="text-muted">{props.date ? new Date(props.date).toLocaleDateString([], options) : ""}</small>
                 </div>
             </div>
         </div>
