f8a446a1ed1f081c6082d56b702f2ddb4528c681
Renan Noronha <renancnoronha@gmail.com>
2022-04-05 17:08:25 -0300

    cleaned google optimize code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,59 +1,37 @@
 import React, { Component } from "react";
 import "./Card.css";
 
-class Card extends Component {
-    async componentDidMount() {
-        if (window.dataLayer) {
-            await window.dataLayer.push({ event: "optimize.activate" });
-        }
-        this.intervalId = setInterval(() => {
-            if (window.google_optimize !== undefined) {
-                // const variant = window.google_optimize.get('uRHJ6tvTTvuFIxOnNMWzNw');
-                const variant = 1;
-                this.setState({ variant });
-                clearInterval(this.intervalId);
-            }
-        }, 100);
-    }
-
-    render() {
-        const options = {
-            year: "numeric",
-            month: "numeric",
-            day: "numeric",
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric",
-            timeZoneName: "short",
-        };
-        return (
-            <div className="card mb-4 box-shadow">
-                {this.state?.variant ? (
-                    <img className="card-img-top" src={this.props.src ? this.props.src : "placeholder_image.jpg"} alt={this.props.alt} />
-                ) : this.props.src ? (
-                    <img className="card-img-top" src={this.props.src} alt={this.props.alt} />
-                ) : (
-                    ""
-                )}
-                <div className="card-body">
-                    <h5>{this.props.title}</h5>
-                    <p className="card-text">{this.props.details}</p>
-                    <div className="d-flex justify-content-between align-items-center">
-                        <div className="btn-group">
-                            {this.props.live && (
-                                <a target="_blank" rel="noreferrer" href={this.props.live}>
-                                    <button type="button" className="btn btn-sm btn-outline-secondary">
-                                        Assistir Lançamento
-                                    </button>
-                                </a>
-                            )}
-                        </div>
-                        <small className="text-muted">{this.props.date ? new Date(this.props.date).toLocaleDateString([], options) : ""}</small>
+function Card() {
+    const options = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        timeZoneName: "short",
+    };
+    return (
+        <div className="card mb-4 box-shadow">
+            <img className="card-img-top" src={this.props.src ? this.props.src : "placeholder_image.jpg"} alt={this.props.alt} />
+            <div className="card-body">
+                <h5>{this.props.title}</h5>
+                <p className="card-text">{this.props.details}</p>
+                <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                        {this.props.live && (
+                            <a target="_blank" rel="noreferrer" href={this.props.live}>
+                                <button type="button" className="btn btn-sm btn-outline-secondary">
+                                    Assistir Lançamento
+                                </button>
+                            </a>
+                        )}
                     </div>
+                    <small className="text-muted">{this.props.date ? new Date(this.props.date).toLocaleDateString([], options) : ""}</small>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Card;
